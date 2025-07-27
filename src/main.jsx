384b9102abc971f832e6b1490aba00e2e9e3d642
NobleStudioz <noblepedro59@gmail.com>
2025-07-27 12:47:34 +0100

    Add main.jsx as Vite entry point for app rendering

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"; // Optional: only if you have styling here
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
